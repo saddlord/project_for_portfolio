ba2e144f8959999ac31238fd6ee0e1568fc3d413
assouev <bsngr.asv@gmail.com>
2020-02-02 20:52:49 +0100

    try to debug heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+    config.action_mailer.default_url_options = { :host => 'project-for-us.herokuapp.com' }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -91,5 +93,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'project-for-us.herokuapp.com' }
 end
