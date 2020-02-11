cc4438117c52a94fd7e3096f17f9680a3a384d56
assouev <bsngr.asv@gmail.com>
2020-02-11 19:48:05 +0100

    debug

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :attendances
+    has_many :attendances
 	has_many :events, through: :attendances
 
   def welcome_send
