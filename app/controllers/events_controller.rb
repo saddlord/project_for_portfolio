520363e9c45035f70ccfeee7c4d78ca9ba575dc3
assouev <bsngr.asv@gmail.com>
2020-02-24 20:21:05 +0100

    page dacceuil + event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,35 @@
 class EventsController < ApplicationController
+	def index
+		
+	end
+
+	def show
+		@event = Event.find(params[:id])
+	end
+
+	def new
+		@event = Event.new(params[:id])
+	end
+
+	def create
+		@event = Event.new(title: params[:title], start_date: params[:start_date], duration: params[:duration], price: params[:price], location: params[:location] )
+		if @event.save
+		redirect_to
+			event_path(event.id)
+		else
+			render :new
+		end
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Event.destroy_all
+8.times do |t|
+    user = Event.create(title: "Plongée", description: "tres jolie plongeoire pour la miff", start_date: 12/12/12, price: 25, location: "paris")
+    
+end
+puts "8 users et items on été créé"
