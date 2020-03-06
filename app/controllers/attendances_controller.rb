3475672c0e4a9c2a4da5244fac0746a576d5e8fd
assouev <bsngr.asv@gmail.com>
2020-03-06 03:50:27 +0100

    attendance rej

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,2 +1,36 @@
 class AttendancesController < ApplicationController
+	
+	def index
+		
+	end
+
+	def new
+		@attendance = Attendance.new(params[:id])
+	end
+
+	def create
+	    @attendance = Attendance.new(user: current_user, stripe_customer_id: params[:token], event: Event.find(params[:event]))
+		if @attendance.save 
+			redirect to 
+			event_path(event.id)
+		else 
+			render :new
+		end
+	end
+
+	def show
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
 end
