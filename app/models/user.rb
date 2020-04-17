4713dae7162155addcf0b1626b9726468bacf5cc
saddlord <bsngr.asv@gmail.com>
2020-04-17 22:12:39 +0200

    active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
     has_many :attendances
 	has_many :events, through: :attendances
   has_many :events, foreign_key: 'admin_id', class_name: "Event"
+  has_one_attached :avatar
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#index'
-  resources :users
+  resources :users do 
+  	resources :avatars, only: [:create]
+  end
   resources :events do 
   	resources :attendances, except: [:new, :show, :edit] do 
   		resources :charges
